2b2eb138cb85239373541aea805467a5ad3c63ea
Infincoding <Exocrophos@gmail.com>
2022-08-04 18:54:57 +0300

    Add xml comments. Changed variable name. Fixed functions. Created functions for print.

--- BankClient/UserInformation.cs
+++ BankClient/UserInformation.cs
@@ -1,24 +1,18 @@
-﻿using System;
-
-namespace BankClient;
+﻿namespace BankClient;
 
 public class UserInformation
 {
-    public UserInformation()
-    {
-        Id = Guid.NewGuid();
-    }
+
+
     public UserInformation(string firstName, string lastName, string phoneNumber)
     {
         FirstName = firstName;
         LastName = lastName;
         PhoneNumber = phoneNumber;
-        Id = Guid.NewGuid();
+        // ID = Guid.NewGuid();
     }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string PhoneNumber { get; set; }
-    public Guid Id { get; set; }
- 
-  
+    public Guid ID { get; set; }
 }
