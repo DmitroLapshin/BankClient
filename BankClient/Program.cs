ce2378c56b505da163dba1a897c30b250cc7a739
Dmitry Lapshin <Exocrophos@gmail.com>
2022-08-11 14:54:05 +0300

    Rename Main on Program

--- BankClient/Program.cs
+++ BankClient/Program.cs
@@ -2,25 +2,5 @@
 
 using BankClient;
 
-var _bank = new Bank();
-var _userInformation = new UserInformation();
-Console.WriteLine("Add User");
-_bank.AddUserInformation();
-_bank.PrintAllUsers();
-
-Console.WriteLine("Get information about one User");
-_bank.GetOneUserInformation(_userInformation.GetInformationFromUser());
-
-Console.WriteLine("Get information about Users");
-_bank.GetAllUsersInformation(_userInformation.GetInformationFromUser());
-
-Console.WriteLine("Search user by ID");
-_bank.GetUserById();
-
-Console.WriteLine("Delete User, Enter Id");
-_bank.DeleteUserInformation();
-_bank.PrintAllUsers();
-
-Console.WriteLine("Change user, Enter just Id");
-_bank.ChangeUserInformation();
-_bank.PrintAllUsers();
+var test = new Bank();
+test.Chooseaction();
