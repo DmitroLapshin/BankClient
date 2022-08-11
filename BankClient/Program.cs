765738a4eb2b805f5f56e443648f2fe88be0630a
Dmitry Lapshin <Exocrophos@gmail.com>
2022-08-11 19:22:26 +0300

    Fixed most remarks after pull request

---
+++ BankClient/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BankClient;
+
+var _bank = new Bank();
+var _userInformation = new UserInformation();
+Console.WriteLine("Add User");
+_bank.AddUserInformation();
+_bank.PrintAllUsers();
+
+Console.WriteLine("Get information about one User");
+_bank.GetOneUserInformation(_userInformation.GetInformationFromUser());
+
+Console.WriteLine("Get information about Users");
+_bank.GetAllUsersInformation(_userInformation.GetInformationFromUser());
+
+Console.WriteLine("Search user by ID");
+_bank.GetUserById();
+
+Console.WriteLine("Delete User, Enter Id");
+_bank.DeleteUserInformation();
+_bank.PrintAllUsers();
+
+Console.WriteLine("Change user, Enter just Id");
+_bank.ChangeUserInformation();
+_bank.PrintAllUsers();
